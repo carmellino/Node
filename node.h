4945aea067a50167dac04fc697a98d34e2c1991a
carmellino <dieveg2000@gmail.com>
2020-11-20 11:21:11 +0100

    zmiany

--- node.h
+++ node.h
@@ -23,7 +23,7 @@ template <typename Type> void Delete(node<Type> *& H){
 }
 
 
-template <typename Type> void Show(node<Type> *& H){
+template <typename Type> void Show(node<Type> * H){
     while(H!=NULL){
         cout<<H->val<<" ";
         H = H->next;
@@ -48,6 +48,80 @@ template <typename Type> void DeleteChosen(node<Type> *& H, Type x){
                 Delete<Type>(tmp->next);
         }
     }
-
 }
 
+
+template <typename Type> void Merge(node<Type> *& A, node<Type> *&  B){
+
+    if(A!=NULL){
+        node<Type> * a = NULL;
+        a = A;
+        while(a->next!=NULL){
+            a=a->next;
+           
+        }
+        a->next = B;
+    }
+    else{
+        A=B;
+    }
+    B=NULL;
+    
+}
+
+
+template <typename Type> void Swap(node<Type> *& h){
+    if(!(h && h->next))
+        return;
+    node<Type> *tmp =NULL;
+    tmp = h;
+    h = tmp->next;
+    tmp->next = h->next;
+    h->next = tmp;
+}
+
+
+template <typename Type> void SwapX(node<Type> *& h, node<Type> * wsk){
+    if(!(h && h->next))
+        return;
+    if(wsk == NULL)
+        Swap<Type>(h);
+    else{
+        node<Type> *tmp = wsk->next;
+        wsk->next = tmp->next;
+        tmp->next = tmp->next->next;
+        wsk->next->next = tmp;
+    }
+}
+
+
+
+
+
+
+
+
+template <typename Type> void BubbleSwap(node<Type> *& left, node<Type> *& right, node<Type> *& watchDog){
+    Type a = right->val;
+    right->val = left->val;
+    left->val = a;
+    watchDog = right;
+}
+
+
+template <typename Type> void BubbleSort(node<Type> *& H){
+    if(H==NULL)
+        return;
+    node<Type> * i = H;
+    node<Type> * j;
+    node<Type> * watchDog = NULL;
+    while(i != NULL){
+        j=H;
+        while((j->next!=watchDog)&&(j->next!=NULL)){
+            if(j->val>j->next->val)
+                BubbleSwap(j,j->next,watchDog);
+            j=j->next;
+        }
+        i=i->next;
+    }
+}
